218de364537baf114082405675638fbfae9fa8b1
wowbob396 <wowbob396@gmail.com>
2018-07-27 18:36:03 -0500

    added second page!

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,14 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    
+    <div>
     <Link to="/page-2/">Go to page 2</Link>
+    </div>
+
+    <div>
+    <Link to="/counter/">Counter Page</Link>
+    </div>
   </div>
 )
 
