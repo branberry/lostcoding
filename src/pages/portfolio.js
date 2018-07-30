32642d53b34ba0aecee4184c1d5fbb77b5b93e6f
wowbob396 <wowbob396@gmail.com>
2018-07-29 21:43:58 -0500

    updated nav bar

---
+++ src/pages/portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+const PortfolioPage = () => (
+  <div style={{ margin: '3rem auto', maxWidth: 600}}>
+    <h4>My Portfolio</h4>
+    <div>
+      <p>
+        Get lost in code with me!
+      </p>
+    </div>
+  </div>
+)
+
+export default PortfolioPage
