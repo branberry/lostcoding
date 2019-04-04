2e966617e94aa9e360443219bf506509f22dd9cb
Brandon Ly <wowbob396@gmail.com>
2019-04-04 14:54:26 -0500

    added material theme

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Lost Coding`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -11,6 +11,12 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        // Add any options here
       },
     },
     `gatsby-transformer-sharp`,
