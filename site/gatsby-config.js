0996c412f75eb5a606803820bc257009456ff932
Brandon Ly <wowbob396@gmail.com>
2019-04-16 15:04:09 -0500

    updated config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -39,6 +39,14 @@ module.exports = {
         
       }
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
