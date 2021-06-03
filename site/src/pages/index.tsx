a8885fcf8d9a127b783a2d9a02b809400d217b50
Brandon Ly <wowbob396@gmail.com>
2021-06-03 09:37:02 -0500

    add basic three render

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { PageProps } from 'gatsby';
+import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+
 import '../styles/index.scss';
-// markup
+
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  const mesh = useRef<THREE.Mesh>(null!)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 const IndexPage: React.FC<PageProps> = props => {
   return (
     <main>
@@ -9,9 +30,16 @@ const IndexPage: React.FC<PageProps> = props => {
       <div className="intro-container">
         <h1>Hi, I'm Brandon!</h1>
       </div>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-3, 0, 0]} />
+        <Box position={[0, 0, 0]} />
+        <Box position={[3, 0, 0]} />
 
+      </Canvas>
     </main>
   )
-}
+};
 
-export default IndexPage
+export default IndexPage;
