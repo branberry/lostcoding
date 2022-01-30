8060157e0eaf0dc0f040c2880d0b0d9de736f1a5
Brandon Ly <brandonly@lostcoding.com>
2022-01-30 14:23:24 -0600

    refactor landing page

--- components/three/scenes/intro.tsx
+++ components/three/scenes/intro.tsx
@@ -5,8 +5,8 @@ function Floor() {
 	const [ref, _] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
 
 	return (
-		<mesh ref={ref}>
-			<meshLambertMaterial color="white" />
+		<mesh receiveShadow ref={ref}>
+			<meshLambertMaterial color="tan" />
 			<planeGeometry args={[100, 100]} />
 		</mesh>
 	);
@@ -16,7 +16,7 @@ export default function Intro() {
 	const [ref, _] = useBox(() => ({ mass: 1 }));
 	return (
 		<>
-			<Text ref={ref} fontSize={2}>
+			<Text castShadow receiveShadow ref={ref} fontSize={2}>
 				Lost Coding
 			</Text>
 			<Floor />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,85 @@
-import { Center, Box } from '@chakra-ui/react'
-import { Canvas } from '@react-three/fiber'
-import MyCanvas from '../components/my-canvas'
-import Intro from '../components/three/scenes/intro'
+import { Center, Box } from '@chakra-ui/react';
+import { Canvas, extend, useFrame, useLoader } from '@react-three/fiber';
+import { Physics } from '@react-three/cannon';
+import { Text, SpotLight, Shadow, Stars, Cloud, Sky } from '@react-three/drei';
+import { useLayoutEffect, useMemo, useRef, useState } from 'react';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { motion as motion3d } from 'framer-motion-3d';
+import { motion } from 'framer-motion';
+import { ErrorBoundary } from 'react-error-boundary';
 
+extend({ TextGeometry });
+
+function ErrorFallback({ error, resetErrorBoundary }: any) {
+	return (
+		<div role="alert">
+			<p>Something went wrong:</p>
+			<pre>{error.message}</pre>
+			<button onClick={resetErrorBoundary}>Try again</button>
+		</div>
+	);
+}
+
+const Banner: React.FC = () => {
+	const font = useLoader(FontLoader, '/static/fonts/bold.blob');
+	const config = useMemo(
+		() => ({
+			font,
+			size: 4,
+			height: 5,
+			curveSegments: 32,
+			bevelEnabled: true,
+			bevelThickness: 0.1,
+			bevelSize: 0.5,
+			bevelOffset: 0,
+			bevelSegments: 8,
+		}),
+		[font]
+	);
+
+	return (
+		<group scale={[0.6, 0.6, 0.4]}>
+			<motion3d.mesh receiveShadow castShadow position={[-15, -2, 0]} whileHover={{ scale: 1.2 }}>
+				<textGeometry args={['LOST', config]} />
+				<meshNormalMaterial />
+				<Shadow position-y={-0.79} rotation-x={-Math.PI / 2} opacity={0.6} scale={[0.8, 0.8, 1]} />
+			</motion3d.mesh>
+			<motion3d.mesh position={[0, -2, 0]} whileHover={{ scale: 1.2 }}>
+				<textGeometry args={['CODING', config]} />
+				<meshNormalMaterial />
+			</motion3d.mesh>
+			<Stars radius={500} depth={50} count={5000} fade />
+		</group>
+	);
+};
+
+const IntroductionSection: React.FC = () => {
+	const transition = {
+		hidden: { opacity: 0 },
+		show: { opacity: 1, delay: 5.0 },
+	};
+	return (
+		<Center>
+			<motion.h1 initial="hidden" animate="show" variants={transition}>
+				Hello! My name is Brandon.
+			</motion.h1>
+		</Center>
+	);
+};
 export default function Home() {
 	return (
-		<Box bg="background">
-			<Center height="100vh">
-				<MyCanvas />
-			</Center>
+		<Box>
+			<Box>
+				<Canvas>
+					<Physics>
+						<ambientLight intensity={4} />
+						<pointLight position={[0, 0, -2]} />
+						<Banner />
+					</Physics>
+				</Canvas>
+			</Box>
+			<IntroductionSection />
 		</Box>
-	)
+	);
 }
