eac15f609913a704e6e1ea961180e735b26fffdf
Brandon Ly <brandonly@lostcoding.com>
2022-04-10 19:17:48 -0500

    update css for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,6 +139,13 @@ export default function Home() {
 				height: '100vh',
 				minWidth: '500px',
 				minHeight: '800px',
+				'-webkit-touch-callout': 'none' /* iOS Safari */,
+				'-webkit-user-select': 'none' /* Safari */,
+				'-khtml-user-select': 'none' /* Konqueror HTML */,
+				'-moz-user-select': 'none' /* Old versions of Firefox */,
+				'-ms-user-select': 'none' /* Internet Explorer/Edge */,
+				'user-select': 'none' /* Non-prefixed version, currently
+				                          supported by Chrome, Edge, Opera and Firefox */,
 			}}>
 			<Canvas>
 				<Physics>
